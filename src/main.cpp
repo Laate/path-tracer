0436b7f34ce71c33ddf65c49dc008001161ff421
Lauri <->
2022-01-31 22:55:08 +0200

    Add multithreading

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <array>
 #include <chrono>
-#include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <omp.h>
+#include <valarray>
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../libs/stb_image_write.h"
 
@@ -17,7 +19,7 @@ auto main(int argc, char **argv) -> int
 {
     std::vector<std::string> arguments(argv + 1, argv + argc);
 
-    constexpr int samples_per_pixel = 2;
+    constexpr int samples_per_pixel = 64;
     constexpr float sample_weight = 1.0F / samples_per_pixel;
     constexpr int max_bounces = 10;
     constexpr int color_channels = 3;
@@ -28,13 +30,24 @@ auto main(int argc, char **argv) -> int
     std::vector<uint8_t> pixels(height * width * color_channels);
 
     auto scene = Scene();
-    if (!gltf::load_gltf("../models/triangles.gltf", scene))
+    if (!gltf::load_gltf("../models/cornell-box.gltf", scene))
     {
         std::cerr << "Loading scene failed." << std::endl;
         exit(1);
     }
 
+    std::valarray<unsigned> progress_counter(omp_get_max_threads());
+    auto print_progress = [&progress_counter]()
+    {
+        int percent = 100.0f * progress_counter.sum() / height;
+        std::cout << "\033[2K\r"
+                  << "[" << std::string(percent / 2, '=')
+                  << std::string(50 - (percent) / 2, ' ') << "] " << percent
+                  << "%, " << omp_get_max_threads() << " threads" << std::flush;
+    };
+
     auto start_time = std::chrono::system_clock::now();
+#pragma omp parallel for schedule(static, 1)
     for (int y = 0; y < height; ++y)
     {
         for (int x = 0; x < width; ++x)
@@ -53,13 +66,12 @@ auto main(int argc, char **argv) -> int
                 pixels[y * width * color_channels + x * color_channels + c] += math::clamp(pixel_color[c], 0.0F, 1.0F) * 255;
             }
         }
-        // Progress bar
-        int percent = ceil(static_cast<float>(100 * y) / height);
-        std::cout << "\033[2K\r"
-                  << "[" << std::string(percent / 2, '=')
-                  << std::string(50 - (percent) / 2, ' ') << "] " << percent
-                  << "%" << std::flush;
+        ++progress_counter[omp_get_thread_num()];
+        if (omp_get_thread_num() == 0)
+            print_progress();
     }
+    print_progress();
+
     auto end_time = std::chrono::system_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
 
